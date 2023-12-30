38cd012431629079597e6b688cbc454e85de15e9
Nhac Tat Nguyen <nhactat.nguyen_ext@zooplus.com>
2023-12-30 15:53:48 +0100

    Read first app property

--- src/main/java/com/ntnguyen/pground/springboot/controller/GreetingController.java
+++ src/main/java/com/ntnguyen/pground/springboot/controller/GreetingController.java
@@ -1,13 +1,17 @@
 package com.ntnguyen.pground.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
+    @Value("${author.name}")
+    private String authorName;
+
     @GetMapping
     public String greeting() {
-        return "Hello World!";
+        return "Hello " + authorName + "!";
     }
 }
